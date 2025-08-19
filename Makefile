8ac08b15acd0b87db31a48246daeb067e5e42e7f
Carlos <dev_carlos@outlook.com.br>
2025-08-18 22:38:26 -0300

    Initial

---
+++ Makefile
@@ -0,0 +1,48 @@
+.POSIX:
+.PHONY: *
+.EXPORT_ALL_VARIABLES:
+
+KUBECONFIG = $(shell pwd)/metal/kubeconfig.yaml
+KUBE_CONFIG_PATH = $(KUBECONFIG)
+
+default: metal system external smoke-test post-install clean
+
+configure:
+	./scripts/configure
+	git status
+
+metal:
+	make -C metal
+
+system:
+	make -C system
+
+external:
+	make -C external
+
+smoke-test:
+	make -C test filter=Smoke
+
+post-install:
+	@./scripts/hacks
+
+# TODO maybe there's a better way to manage backup with GitOps?
+backup:
+	./scripts/backup --action setup --namespace=actualbudget --pvc=actualbudget-data
+	./scripts/backup --action setup --namespace=jellyfin --pvc=jellyfin-data
+
+restore:
+	./scripts/backup --action restore --namespace=actualbudget --pvc=actualbudget-data
+	./scripts/backup --action restore --namespace=jellyfin --pvc=jellyfin-data
+
+test:
+	make -C test
+
+clean:
+	docker compose --project-directory ./metal/roles/pxe_server/files down
+
+docs:
+	mkdocs serve
+
+git-hooks:
+	pre-commit install
---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Homelab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            ansible
+            ansible-lint
+            bmake
+            diffutils
+            docker
+            docker-compose
+            dyff
+            git
+            glibcLocales
+            go
+            gotestsum
+            iproute2
+            jq
+            k9s
+            kanidm
+            kube3d
+            kubectl
+            kubernetes-helm
+            kustomize
+            libisoburn
+            neovim
+            openssh
+            opentofu # Drop-in replacement for Terraform
+            p7zip
+            pre-commit
+            qrencode
+            shellcheck
+            wireguard-tools
+            yamllint
+
+            (python3.withPackages (p: with p; [
+              jinja2
+              kubernetes
+              mkdocs-material
+              netaddr
+              pexpect
+              rich
+            ]))
+          ];
+        };
+      }
+    );
+}
